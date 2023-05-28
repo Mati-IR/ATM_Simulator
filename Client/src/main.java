265023c43f7787f50aa13b942a4e16ff3cc4437c
Mati-IR <mpabich4@gmail.com>
2023-05-28 12:59:27 +0200

    Cleanup after refactorization

--- Client/src/main.java
+++
@@ -1,8 +0,0 @@
-import com.socket.ATMClient;
-
-public class main {
-    public static void main(String[] args) {
-        ATMClient atmClient = new ATMClient(42, "localhost", 1234);
-        
-    }
-}
